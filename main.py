004d96efc58e9a5863e2ff892e417ff4348027ae
Dávid Bartal <35382618+bartaldavid@users.noreply.github.com>
2024-08-27 20:05:10 +0200

    add: basic pages

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import HTMLResponse
 from recipe_scrapers import scrape_html
 import requests
 import validators
+from pydantic import BaseModel
+from fastapi.templating import Jinja2Templates
+
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+
+class RecipeJSON(BaseModel):
+    title: str
+    author: str
+    cook_time: int | None
+    host: str
+    total_time: int | None
+    image: str
+    ingredients: list[str]
+    # ingredient_groups: AbstractScraper.ingredient_groups
+    instructions: str
+    instructions_list: list[str]
+    language: str
+    site_name: str
+    image: str
 
 
 @app.get("/recipe")
-def read_item(url: str):
+def read_item(url: str) -> RecipeJSON:
     if not validators.url(url):
         raise HTTPException(status_code=400, detail="Invalid URL")
 
